47800aba3b352a1b2a4e872cde7c369dae65eb65
Mark Yuan <mark@webasone.com>
2026-02-09 19:30:55 -0800

    use asone id

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+declare global {
+  interface Window {
+    asoneId: string;
+  }
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById(window.asoneId));
 
 root.render(<App />);
